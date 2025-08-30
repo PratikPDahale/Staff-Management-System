28c81bf610e586082f1528c61a2b21a7c798a534
PratikPDahale <dahalepratik2004@gmail.com>
2025-08-30 19:24:06 +0530

    first commit

---
+++ ems-backend/ems-backend/src/main/java/com/practice/ems/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.practice.ems.service;
+
+import com.practice.ems.dto.EmployeeDto;
+
+import java.util.List;
+
+public interface EmployeeService {
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
+
+    EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployee);
+
+    void deleteEmployee(Long employeeId);
+}
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark' style={{backgroundColor: '#f11e72ff'}}>
+            <a className='navbar-brand' href="/">Staff Management System</a>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
